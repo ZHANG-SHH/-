8f0516809e5233c9c3871968200ad683def9f6ca
zkh_0 <zkh_0@DESKTOP-SD88OD1>
2019-08-18 23:37:37 +0800

    LU A.

--- TeamWork/TeamWork/MainWindow.xaml.cs
+++ TeamWork/TeamWork/MainWindow.xaml.cs
@@ -73,9 +73,31 @@ namespace TeamWork
                     else sum = dateTime1.DayOfYear - dateTime2.DayOfYear;
                     HasBorn.Text = sum.ToString();
                 }
-                //万天
 
-             }
+                //一万天判断
+                int summ = YearDays(Convert.ToInt32(year)) - dateTime2.DayOfYear;
+                int i = Convert.ToInt32(year);
+                DateTime dateTime = new DateTime(i, month, date);
+                for (i = Convert.ToInt32(year) + 1; 10000 - summ >= YearDays(i) - dateTime.DayOfYear; i++)
+                {
+                    summ += YearDays(i);
+                }
+
+                int dayy = dateTime.DayOfYear + 10000 - summ;
+                for (int j = 1; j <= 12; j++)
+                {
+                    for (int k = 1; k <= MonthDays(i, j); k++)
+                    {
+                        DateTime dateTime3 = new DateTime(i, j, k);
+                        if (dateTime3.DayOfYear == dayy)
+                        {
+                            万天计划.Text = i.ToString() + "." + j.ToString() + "." + k.ToString();
+                            break;
+                        }
+                    }
+                }
+
+            }
         }
 
 
@@ -84,6 +106,33 @@ namespace TeamWork
             if (DateTime.IsLeapYear(year))
                 return 366;
             else return 365;
+        }
+
+        static int MonthDays(int year, int month)
+        {
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                default:
+                    {
+                        if (DateTime.IsLeapYear(year))
+                            return 29;
+                        else return 28;
+                    }
+
+            }
         }
 
         static bool CompareDate(int a, int b, int c, int d)
